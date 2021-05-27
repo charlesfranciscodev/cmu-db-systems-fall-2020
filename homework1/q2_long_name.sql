be1de30caa1fd13afee85060c3b6652cd28bd424
Charles-A. Francisco <charlantfr@gmail.com>
2021-05-27 19:19:25 -0400

    homework 1 - q4

--- homework1/q2_long_name.sql
+++ homework1/q2_long_name.sql
@@ -1,8 +1,8 @@
--- List works with longest name of each type.
--- Details: For each work type, find works that have the longest names.
--- There might be cases where there is a tie for the longest names - in that case, return all of them.
--- Display work names and corresponding type names,
--- and order it according to work type (ascending) and use work name (ascending) as tie-breaker.
+/* List works with longest name of each type.
+Details: For each work type, find works that have the longest names.
+There might be cases where there is a tie for the longest names - in that case, return all of them.
+Display work names and corresponding type names,
+and order it according to work type (ascending) and use work name (ascending) as tie-breaker. */
 SELECT work.name, work_type.name
 FROM work
 JOIN work_type ON work.type = work_type.id
--- homework1/q3_old_music_nations.sql
+++ homework1/q3_old_music_nations.sql
@@ -1,7 +1,7 @@
--- List top 10 countries with the most classical music artists
--- (born or started before 1850) along with the number of associated artists.
--- Details: Print country and number of associated arists before 1850.
--- For example, Russia|191. Sort by number of artists in descending order.
+/* List top 10 countries with the most classical music artists
+(born or started before 1850) along with the number of associated artists.
+Details: Print country and number of associated arists before 1850.
+For example, Russia|191. Sort by number of artists in descending order. */
 SELECT area.name, COUNT(area.name) AS nb_artists
 FROM area
 JOIN artist ON area.id = artist.area
